2ea635dbf078258a995ea80c01cee95382243677
dimasrifalta <dimasrifalta@gmail.com>
2022-03-26 22:41:36 +0700

    creata all fitur

--- utils/fetchApi.js
+++ utils/fetchApi.js
@@ -5,9 +5,9 @@ export const baseUrl = 'https://bayut.p.rapidapi.com';
 export const fetchApi = async (url) => {
   const { data } = await axios.get((url), {
     headers: {
-      'x-rapidapi-host': 'bayut.p.rapidapi.com',
-      'x-rapidapi-key': process.env.NEXT_PUBLIC_RAPID_API_KEY ,
-    },
+      'X-RapidAPI-Host': 'bayut.p.rapidapi.com',
+      'X-RapidAPI-Key': 'ef5857d1d5msh6611be9f9d326e0p138e76jsn9420dfaf47e8'
+    }
   });
     
   return data;
